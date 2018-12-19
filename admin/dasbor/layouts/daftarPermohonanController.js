17f9eb124b557b6d00b6435c5ad3417307110190
Z-Fikar <zulfikar97@gmail.com>
2018-12-20 06:32:01 +0700

    Add fitur pencarian di admin daftar permohonan

--- admin/dasbor/layouts/daftarPermohonanController.js
+++ admin/dasbor/layouts/daftarPermohonanController.js
@@ -3,6 +3,42 @@ if (window == window.top) {
 }
 
 window.onload = function() {
+  var list = [];
+  var generateTable = function(d) {
+    var head = [
+      "ID",
+      "TanggalPermohonan",
+      "NamaLengkap",
+      "JenisPermohonan",
+      "ID"
+    ];
+    var tbody = document.querySelector(".col table tbody");
+    while (tbody.firstChild) {
+      tbody.removeChild(tbody.firstChild);
+    }
+    for (i = 0; i < d.length; i++) {
+      var tr = document.createElement("TR");
+      for (j = 0; j < head.length; j++) {
+        var td = document.createElement("TD");
+        if (j < head.length - 1) {
+          var txt = document.createTextNode(d[i][head[j]]);
+          td.appendChild(txt);
+        } else {
+          var btn = document.createElement("BUTTON");
+          var txt = document.createTextNode("SUNTING");
+          btn.appendChild(txt);
+          btn.data = d[i][head[j]];
+          btn.classList.add("btn-border");
+          btn.onclick = function() {
+            location.href = "detail-permohonan.html?id=" + this.data;
+          };
+          td.appendChild(btn);
+        }
+        tr.appendChild(td);
+      }
+      tbody.appendChild(tr);
+    }
+  };
   var getAll_permohonan = function() {
     if (window.XMLHttpRequest) {
       var hr = new XMLHttpRequest();
@@ -19,36 +55,8 @@ window.onload = function() {
         console.log(hr);
         var data = JSON.parse(hr.response);
         if (data.SuccessMessage) {
-          var d = data.List;
-          var head = [
-            "TanggalPermohonan",
-            "NamaLengkap",
-            "JenisPermohonan",
-            "ID"
-          ];
-          var tbody = document.querySelector(".col table tbody");
-          for (i = 0; i < d.length; i++) {
-            var tr = document.createElement("TR");
-            for (j = 0; j < head.length; j++) {
-              var td = document.createElement("TD");
-              if (j < head.length - 1) {
-                var txt = document.createTextNode(d[i][head[j]]);
-                td.appendChild(txt);
-              } else {
-                var btn = document.createElement("BUTTON");
-                var txt = document.createTextNode("SUNTING");
-                btn.appendChild(txt);
-                btn.data = d[i][head[j]];
-                btn.classList.add("btn-border");
-                btn.onclick = function() {
-                  location.href = "detail-permohonan.html?id=" + this.data;
-                };
-                td.appendChild(btn);
-              }
-              tr.appendChild(td);
-            }
-            tbody.appendChild(tr);
-          }
+          list = data.List;
+          generateTable(list);
         } else {
           alert(data.InfoMessage);
         }
@@ -57,6 +65,24 @@ window.onload = function() {
     hr.setRequestHeader("Content-type", "application/json");
     hr.send(JSON.stringify(params));
   };
-
   getAll_permohonan();
+
+  var cariPermohonan = function(str) {
+    var res = list.filter(function(item) {
+      console.log(item.NamaLengkap.indexOf(str));
+      return (
+        item.NamaLengkap.toLowerCase().indexOf(str.toLowerCase()) >= 0 ||
+        item.TanggalPermohonan.toLowerCase().indexOf(str.toLowerCase()) >= 0 ||
+        item.JenisPermohonan.toLowerCase().indexOf(str.toLowerCase()) >= 0 ||
+        item.ID.toLowerCase().indexOf(str.toLowerCase()) >= 0
+      );
+    });
+    generateTable(res);
+  };
+
+  var cari = document.getElementById("btnCari");
+  cari.onclick = function() {
+    var str = document.getElementById("txtCari").value;
+    cariPermohonan(str);
+  };
 };

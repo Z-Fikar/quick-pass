86b9314b77611e41185eb4ca0415bd9f515e95e1
Z-Fikar <zulfikar97@gmail.com>
2018-12-22 16:52:27 +0700

    Update add loading

--- user/dasbor/layouts/indexController.js
+++ user/dasbor/layouts/indexController.js
@@ -3,7 +3,9 @@ if(window==window.top){
 }
 
 window.onload = function() {
+  var l = document.querySelector(".overlay");
   var get_profil = function() {
+    l.style.display = "block";
     if (window.XMLHttpRequest) {
       var hr = new XMLHttpRequest();
     } else {
@@ -18,9 +20,13 @@ window.onload = function() {
       if (hr.readyState == 4 && hr.status == 200) {
         console.log(hr);
         var data = JSON.parse(hr.response);
+        l.style.display = "none";
         if (data.SuccessMessage) {
           var d = data.List;
           var namaLengkap = document.getElementById("lblNamaLengkap");
+          if(d.NamaLengkap==""){
+            d.NamaLengkap = "silahkan lengkapi data Profil anda di sini."
+          }
           namaLengkap.appendChild(document.createTextNode(d.NamaLengkap));
         } else {
           alert(data.InfoMessage);
